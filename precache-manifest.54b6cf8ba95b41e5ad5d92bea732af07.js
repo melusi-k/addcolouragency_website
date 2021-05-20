28780ab38cb01dfb95efb2d13abdb5042781fdb9
Melusi Khumalo <melusi.l.k@gmail.com>
2021-05-20 12:33:20 +0200

    Updates

---
+++ precache-manifest.54b6cf8ba95b41e5ad5d92bea732af07.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a00a92ae4aa849b8a7a17ece398bea5b",
+    "url": "/addcolouragency_website/index.html"
+  },
+  {
+    "revision": "8c6c4d574997ff745097",
+    "url": "/addcolouragency_website/static/css/main.e34e5576.chunk.css"
+  },
+  {
+    "revision": "b50beb2365b6e9dda1fc",
+    "url": "/addcolouragency_website/static/js/2.2428bece.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/addcolouragency_website/static/js/2.2428bece.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c6c4d574997ff745097",
+    "url": "/addcolouragency_website/static/js/main.12c84b88.chunk.js"
+  },
+  {
+    "revision": "c4bc791f6de044379b03",
+    "url": "/addcolouragency_website/static/js/runtime-main.e01183d5.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/addcolouragency_website/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/addcolouragency_website/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/addcolouragency_website/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/addcolouragency_website/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/addcolouragency_website/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/addcolouragency_website/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/addcolouragency_website/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/addcolouragency_website/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/addcolouragency_website/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/addcolouragency_website/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/addcolouragency_website/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/addcolouragency_website/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "a988a1293c75a86dfde1948cf83077b6",
+    "url": "/addcolouragency_website/static/media/logo.a988a129.png"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/addcolouragency_website/static/media/video-placeholder.a622fc5d.jpg"
+  }
+]);
